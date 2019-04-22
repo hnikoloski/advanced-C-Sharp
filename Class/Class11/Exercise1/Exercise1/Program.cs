e7b95251dd13fa21d760ab0cd67c42add44c7092
Hristijan Nikoloski <riki.vs@outlook.com>
2019-04-22 20:45:08 +0200

    class 11 upload

---
+++ Class/Class11/Exercise1/Exercise1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            #region firstExercise
+
+            //Console.WriteLine("Enter your file name: ");
+            //string fileName = Console.ReadLine();
+            //Console.WriteLine("Enter your extension");
+            //string fileExtension = Console.ReadLine();
+            //Console.WriteLine("Enter your text");
+            //string fileText = Console.ReadLine();
+            //string path = $@"C:\Users\Hristijan.nikoloski\Desktop\Hristijan.Nikoloski\advancedC#\Class\Class11\logs\{fileName}.{fileExtension}";
+            //FileInfo bio = new FileInfo(path);
+
+
+            //using (StreamWriter sw = bio.CreateText())
+            //{
+            //    sw.WriteLine(fileText);
+            //}
+
+            //using (StreamReader sr = bio.OpenText())
+            //{
+            //    string log = "";
+            //    while ((log = sr.ReadLine()) != null)
+            //    {
+            //        Console.WriteLine(log);
+            //    }
+            //}
+
+            #endregion
+
+            #region NugetExercise
+
+
+
+            #endregion
+            Console.ReadKey();
+
+        }
+    }
+}
